1057cea4e530c9f68cbac42d2e69b06b61d23192
Karthikvk2 <vkottaryk@gmail.com>
2024-09-12 20:20:22 +0530

    final app

---
+++ Frontend/script.js
@@ -0,0 +1,58 @@
+// send watch data to db
+
+document
+  .getElementById("watch-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formdata = new FormData(this);
+    fetch("http://127.0.0.1:8000/api/watches/create/", {
+      method: "POST",
+      body: formdata,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert("Watch added successfully!");
+        displayWatches();
+      })
+      .catch((error) => console.error("Error", error));
+  });
+
+//To get all the list of Watches
+const baseUrl = "http://127.0.0.1:8000";
+
+function displayWatches() {
+  fetch("http://127.0.0.1:8000/api/watches/")
+    .then((response) => response.json())
+    .then((data) => {
+      const container = document.getElementById("watches-container");
+      container.innerHTML = "";
+      data.forEach((watch) => {
+        const imageUrl = baseUrl + watch.image;
+        const watchCard = `
+            <div class="card_container" id="watch-list">
+                <div class="card" style="width: 18rem">
+                    <img src="${imageUrl}" class="card-img-top" alt="image" />
+                    <div class="card-body">
+                        <p class="card-text">
+                            <h5 class="watch-name" style="text-align:center;">Watch Name: ${watch.name}</h5>
+                            <p class="watch-model" style="text-align:center;">Watch Brand: ${watch.brand}</p>
+                            <h6><p class="watch-price" style="text-align:center;">₹ ${watch.price}</p></h6>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            `;
+        container.innerHTML += watchCard;
+      });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", displayWatches);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop();
+    if (currentPage === "watches.html" || currentPage === "index.html") {
+      displayWatches();
+    }
+  });
+
